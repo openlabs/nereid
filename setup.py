90a26de509e47d99254190cbeb5097208552aea7
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-01-08 16:10:16 +0530

    [FIX]Build setup.py such that it installs tryton module only if trytond is installed. Supports callisto

--- setup.py
+++ setup.py
@@ -9,20 +9,44 @@
 from setuptools import Command, setup
 import re
 
-trytond_module_info = eval(open('trytond_nereid/__tryton__.py').read())
-major_version, minor_version, _ = trytond_module_info.get(
-    'version', '0.0.1').split('.', 2)
-major_version = int(major_version)
-minor_version = int(minor_version)
+try:
+    import trytond
+    tryton_installed = True
+except ImportError:
+    tryton_installed = False
 
 requires = ['flask']
-for dep in trytond_module_info.get('depends', []):
-    if not re.match(r'(ir|res|workflow|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
-requires.append('trytond >= %s.%s, < %s.%s' %
-        (major_version, minor_version, major_version, minor_version + 1))
+package_dir = {'nereid': 'nereid'}
+package_data = { }
+entry_points = ''
+packages = ['nereid',]
+
+
+if tryton_installed:
+    trytond_module_info = eval(open('trytond_nereid/__tryton__.py').read())
+    major_version, minor_version, _ = trytond_module_info.get(
+        'version', '0.0.1').split('.', 2)
+    major_version = int(major_version)
+    minor_version = int(minor_version)
+
+    for dep in trytond_module_info.get('depends', []):
+        if not re.match(r'(ir|res|workflow|webdav)(\W|$)', dep):
+            requires.append('trytond_%s >= %s.%s, < %s.%s' %
+                    (dep, major_version, minor_version, major_version,
+                        minor_version + 1))
+    requires.append('trytond >= %s.%s, < %s.%s' %
+            (major_version, minor_version, major_version, minor_version + 1))
+    package_dir.update({'trytond.modules.nereid': 'trytond_nereid'})
+    package_data.update({
+        'trytond.modules.nereid': trytond_module_info.get('xml', []) \
+                + trytond_module_info.get('translation', []),
+        })
+    entry_points += """
+    [trytond.modules]
+    nereid = trytond.modules.nereid
+    """
+    packages.append('trytond.modules.nereid')
+
 
 setup(
     name='Nereid',
@@ -33,28 +57,16 @@ setup(
     author_email='sharoon.thomas@openlabs.co.in',
     description='Tryton - Web Framework extension',
     long_description=__doc__,
-    package_dir={
-        'trytond.modules.nereid': 'trytond_nereid',
-        'nereid': 'nereid'
-        },
-    package_data={
-        'trytond.modules.nereid': trytond_module_info.get('xml', []) \
-                + trytond_module_info.get('translation', []),
-        },
-    packages=[
-        'nereid',
-        'trytond.modules.nereid',
-        ],
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
     zip_safe=False,
     platforms='any',
     install_requires=[
         'flask',
         'wtforms',
     ],
-    entry_points="""
-    [trytond.modules]
-    nereid = trytond.modules.nereid
-    """,
+    entry_points=entry_points,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -65,8 +77,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    #cmdclass={'audit': run_audit},
-    #test_suite='__main__.run_tests'
 )
 
 
