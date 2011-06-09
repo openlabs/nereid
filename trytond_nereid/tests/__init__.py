871ec80a78ef2caaaea8882e9c2846b064eb7b96
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-06-08 22:08:29 -0400

    Add a consolidated test suite which could be imported by the tryton test suite

---
+++ trytond_nereid/tests/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Nereid Tryton module test cases
+
+    :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from unittest import TestSuite
+
+from .configuration import suite as configuration_test_suite
+from .test_currency import suite as currency_test_suite
+from .test_language import suite as language_test_suite
+
+suite = TestSuite()
+suite.addTests([
+    configuration_test_suite,
+    currency_test_suite,
+    language_test_suite
+    ])
