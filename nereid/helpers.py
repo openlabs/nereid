45c9a270a32b618f2ecf396eabcaf089ca91029e
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-06-14 17:45:36 -0400

    Removes dependency on OTCLTools for pagination fixes #5

--- nereid/helpers.py
+++ nereid/helpers.py
@@ -17,8 +17,8 @@ import re
 import unicodedata
 from functools import wraps
 from hashlib import md5
+from math import ceil
 
-from otcltools.general.pagination import Pagination as BasePagination
 from flask.helpers import _assert_have_json, json, jsonify
 from werkzeug import Headers, wrap_file, redirect
 from werkzeug.exceptions import NotFound
@@ -369,6 +369,131 @@ def make_crumbs(browse_record, endpoint, add_home=True, max_depth=10,
     items.reverse()
 
     return items
+
+
+class BasePagination(object):
+    """
+    General purpose paginator for doing pagination
+
+    With an empty dataset assert the attributes
+    >>> p = Pagination(1, 3, [])
+    >>> p.count
+    0
+    >>> p.pages
+    0
+    >>> p.begin_count
+    0
+    >>> p.end_count
+    0
+
+    Test with a range(1, 10)
+    >>> p = Pagination(1, 3, range(1, 10))
+    >>> p.count
+    9
+    >>> p.all_items()
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> p.pages
+    3
+    >>> p.begin_count
+    1
+    >>> p.end_count
+    3
+
+    """
+
+    def __init__(self, page, per_page, data=None):
+        """
+        :param per_page: Items per page
+        :param page: The page to be displayed
+        :param data: The data table
+        """
+        self.per_page = per_page
+        self.page = page
+        self.data = data if data is not None else []
+
+    @property
+    def count(self):
+        "Returns the count of data"
+        return len(self.data)
+
+    def all_items(self):
+        """Returns complete set of items"""
+        return self.data
+
+    def items(self):
+        """Returns the list of items in current page
+        """
+        return self.data[self.offset:self.offset + self.per_page]
+
+    def __iter__(self):
+        for item in self.items():
+            yield item
+
+    def __len__(self):
+        return self.count
+
+    @property
+    def prev(self):
+        """Returns a :class:`Pagination` object for the previous page."""
+        return Pagination(self.page - 1, self.per_page, self.data)
+
+    def next(self):
+        """Returns a :class:`Pagination` object for the next page."""
+        return Pagination(self.page + 1, self.per_page, self.data)
+
+    #: Attributes below this may not require modifications in general cases
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                    right_current=2, right_edge=2):
+        """Iterates over the page numbers in the pagination.  The four
+        parameters control the thresholds how many numbers should be produced
+        from the sides.  Skipped page numbers are represented as `None`.
+        This is how you could render such a pagination in the templates:
+
+        .. sourcecode:: html+jinja
+
+            {% macro render_pagination(pagination, endpoint) %}
+                <div class=pagination>
+                {%- for page in pagination.iter_pages() %}
+                    {% if page %}
+                        {% if page != pagination.page %}
+                            <a href="{{ url_for(endpoint, page=page) }}">{{ page }}</a>
+                        {% else %}
+                            <strong>{{ page }}</strong>
+                        {% endif %}
+                    {% else %}
+                        <span class=ellipsis>…</span>
+                    {% endif %}
+                {%- endfor %}
+                </div>
+            {% endmacro %}
+        """
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+                (num > self.page - left_current - 1 and \
+                 num < self.page + right_current) or \
+                num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
+
+    offset = property(lambda self: (self.page - 1) * self.per_page)
+
+    prev_num = property(lambda self: self.page - 1)
+    has_prev = property(lambda self: self.page > 1)
+
+    next_num = property(lambda self: self.page + 1)
+    has_next = property(lambda self: self.page < self.pages)
+
+    pages = property(lambda self: int(ceil(self.count / float(self.per_page))))
+
+    begin_count = property(lambda self: min([
+        ((self.page - 1) * self.per_page) + 1, 
+        self.count]))
+    end_count = property(lambda self: min(
+        self.begin_count + self.per_page - 1, self.count))
 
 
 class Pagination(BasePagination):
