9199f35fbb552e1f6de44a34aa769f29ad13a127
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-06-16 19:20:26 -0400

    Added a test_runner which could be called to run all tests

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Generic tests for nereid.
+
+    :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+
---
+++ tests/test_runner.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+    test_runner
+
+    A test runner which collects all tests from the core nereid module and
+    trytond modules in the directory and executes them
+
+    As tryton itslef is not a dependency of nereid, the tryton tests will be 
+    loaded, only if tryton is installed
+
+    :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+import unittest2
+
+try:
+    import trytond
+    TRYTON_INSTALLED = True
+except ImportError:
+    TRYTON_INSTALLED = False
+
+# Test Suite into which all tests are collected
+suite = unittest2.TestSuite()
+
+# Begin loading tests
+if TRYTON_INSTALLED:
+    from trytond.modules.nereid.tests import suite as _suite
+    suite.addTests([_suite])
+
+
+if __name__ == '__main__':
+    unittest2.TextTestRunner(verbosity=2).run(suite)
