15ab22fe0e4c887499ec6e171cc7c58c379e3d79
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2012-03-14 13:18:50 -0300

    Fix the header and encoding

--- trytond_nereid/party.py
+++ trytond_nereid/party.py
@@ -1,5 +1,4 @@
-# This file is part of Nereid.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
+# -*- coding: UTF-8 -*-
 '''
     nereid_trytond.party
 
