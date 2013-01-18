ae93640fa9720ed6365de880d10d67ad4c544d4e
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-01-18 12:27:57 +0530

    Bump to development version 2.4.0.8dev

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Nereid',
-    version='2.4.0.7',
+    version='2.4.0.8dev',
     url='http://nereid.openlabs.co.in/docs/',
     license='GPLv3',
     author='Openlabs Technologies & Consulting (P) Limited',
