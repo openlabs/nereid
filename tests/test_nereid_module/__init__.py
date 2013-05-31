2102082f440d2159f70ce2bbef71eff1fb3ba2c4
Simmi Anand <simmi.anand@openlabs.co.in>
2013-05-31 13:34:01 +0530

    Migrate Test model of nereid to 2.6 #1519
    * Migrated test_nereid_module.pagination to 2.6
    
    Cherry-picked from 7904853d7d80555dc6012bd3d4b64c18bce083bd

--- tests/test_nereid_module/__init__.py
+++ tests/test_nereid_module/__init__.py
@@ -4,7 +4,18 @@
 
     A test module to test core nereid features where Tryton model is reqd
 
-    :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Limited
+    :copyright: (c) 2011-2013 by Openlabs Technologies & Consulting (P) Limited
     :license: GPLv3, see LICENSE for more details.
 """
-from .model import *
+from trytond.pool import Pool
+from model import PaginationModel
+
+
+def register():
+    """
+    This function will register test nereid module
+    """
+    Pool.register(
+        PaginationModel,
+        module='test_nereid_module', type_='model',
+    )
