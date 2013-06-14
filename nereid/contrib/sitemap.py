8f36c8f041ead9fc633c48a92e163e8da5a2ed2f
Sergi Almacellas Abellana <sergi@koolpi.com>
2013-06-14 10:26:44 +0200

    Fix enconding errors (PEP263)

--- nereid/contrib/sitemap.py
+++ nereid/contrib/sitemap.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #This file is part of Tryton & Nereid. The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from math import ceil
