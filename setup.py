8e6717953c2ff2a7d9f4d3471cfd27064523dd21
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-08-16 11:47:04 +0530

    Release version 2.4.0.11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Nereid',
-    version='2.4.0.10',
+    version='2.4.0.11',
     url='http://nereid.openlabs.co.in/docs/',
     license='GPLv3',
     author='Openlabs Technologies & Consulting (P) Limited',
